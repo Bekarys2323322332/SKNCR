fbb9c028690616db950986cd5fc44fc0643a1f32
Bekarys2323322332 <bekarys.rakhymbek@gmail.com>
2025-11-09 20:41:36 +0000

    d

--- app/(tabs)/scanbar.tsx
+++ app/(tabs)/scanbar.tsx
@@ -109,7 +109,7 @@ const ScanBar = () => {
         };
         console.log("Sending payload to backend:", payload);
 
-        const response = await fetch("https://skncr.vercel.app/api/aiAgent", {
+        const response = await fetch("http://localhost:8081", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify(payload),
